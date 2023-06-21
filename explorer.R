07be3591ce109eaa033e8f3fab05f35ed1044816
Alexander Bürklen <Alexander.Buerklen@Student.HTW-Berlin.de>
2023-06-21 16:25:23 +0200

    code für idee 1

--- explorer.R
+++ explorer.R
@@ -1,5 +1,5 @@
 # embarked: Port of embarkation -> S = Southhampton, C = Cherbourg, 
-# Q = Queenstown
+# Q = Queenstown. Ablauf: Southampton–Cherbourg–Queenstown–New York, New York–Plymouth–Cherbourg–Southampton
 # ideen: 
 # 1.) alter + überlebt -> hat das alter einen einfluss auf die
 # überlebenschance?
@@ -13,4 +13,25 @@
 # überlebenschance?
 # 6.) sibsp + survived -> hatten geschwister/ehepaare eine höhere 
 # überlebenschance?
-# 7.) 
+
+# 1.)
+# Dataframe zusammenführen
+data <- data.frame(survived, age)
+
+# Überlebensstatus als Faktor konvertieren
+data$survived <- factor(data$survived, labels = c("Nein", "Ja"))
+
+# Alter in diskrete Gruppen einteilen
+data$age_group <- cut(data$age, breaks = seq(0, max(data$age, na.rm = TRUE), by = 10), include.lowest = TRUE, labels = FALSE)
+
+# Plot erstellen
+plot_1 <- ggplot(data, aes(x = factor(age_group), fill = survived)) +
+  geom_bar(position = "dodge", width = 0.7) +
+  scale_x_discrete(labels = paste0(seq(0, max(data$age, na.rm = TRUE), by = 10), "-", seq(9, max(data$age, na.rm = TRUE), by = 10))) +
+  labs(x = "Alter", y = "Anzahl", fill = "Überleben") +
+  ggtitle("Häufigkeit des Überlebensstatus nach Alter")
+
+# Plot anzeigen
+print(plot_1)
+# 2.) 
+

0bc5640d9844e8c81fed9519ce0387c0bf06d21b
Alexander Bürklen <Alexander.Buerklen@Student.HTW-Berlin.de>
2023-06-21 08:14:23 +0200

    explorer.r datei und ideen hinzugefügt

---
+++ explorer.R
@@ -0,0 +1,16 @@
+# embarked: Port of embarkation -> S = Southhampton, C = Cherbourg, 
+# Q = Queenstown
+# ideen: 
+# 1.) alter + überlebt -> hat das alter einen einfluss auf die
+# überlebenschance?
+# 2.) fare + survived -> vllt hat ein teureres ticket zu einer höheren 
+# überlebenschance geführt? (bevorzugung von passagieren?)
+# 3.) parch + survived -> hatten eltern und kinder eine höhere 
+# überlebenschance?
+# 4.) pclass + survived -> hatte die passagierklasse einfluss auf die 
+# überlebenschance?
+# 5.) sex + survived -> hatte das geschlecht einfluss auf die 
+# überlebenschance?
+# 6.) sibsp + survived -> hatten geschwister/ehepaare eine höhere 
+# überlebenschance?
+# 7.) 

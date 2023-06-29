ce948458de094fb51b197ae4109be1a2369633da
Alexander Bürklen <Alexander.Buerklen@Student.HTW-Berlin.de>
2023-06-29 16:42:24 +0200

    neu

--- app.R
+++ app.R
@@ -1,41 +1,114 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-
-
 library(shiny)
-
+library(ggplot2)
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bin_input", "Bin-Breite:", min = 1, max = 20, value = 1),
+      selectInput("survived_input", "Überlebensstatus:",
+                  choices = c("Alle", "Überlebt", "Nicht-Überlebt")),
+      sliderInput("bin_input", "Bin-Breite:", min = 1, max = 10, value = 1)
     ),
     mainPanel(
       plotOutput("histogram")
     )
   )
 )
-
 server <- function(input, output) {
   output$histogram <- renderPlot({
-    ggplot(data_1_clean, aes(x = age, fill = factor(survived))) +
+    filtered_data <- switch(input$survived_input,
+                            "Alle" = data_1,
+                            "Überlebt" = data_1[data_1$survived == "Ja", ],
+                            "Nicht-Überlebt" = data_1[data_1$survived == "Nein", ])
+    ggplot(filtered_data, aes(x = age, fill = factor(survived))) +
       geom_histogram(binwidth = input$bin_input, color = "black") +
-      scale_x_continuous(breaks = seq(0, max(data_1_clean$age, na.rm = TRUE), by = 1),
-                         labels = seq(0, max(data_1_clean$age, na.rm = TRUE), by = 1)) +
+      scale_x_continuous(breaks = seq(0, max(data_1$age, na.rm = TRUE), by = 1),
+                         labels = seq(0, max(data_1$age, na.rm = TRUE), by = 1)) +
       labs(x = "Alter", y = "Anzahl", fill = "Überlebensstatus") +
       ggtitle("Aufteilung der Anzahl der Personen, die überlebt haben oder nicht") +
       scale_fill_manual(values = c("red", "green"), labels = c("Nicht-Überlebt", "Überlebt"), name = "Überlebensstatus")
   })
 }
-
 shinyApp(ui, server)
-
-
-
-
-
-
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("survived_input", "Überlebensstatus:",
+                  choices = c("Alle", "Überlebt", "Nicht-Überlebt")),
+      sliderInput("bin_input", "Bin-Breite:", min = 1, max = 10, value = 1)
+    ),
+    mainPanel(
+      plotOutput("histogram")
+    )
+  )
+)
+server <- function(input, output) {
+  output$histogram <- renderPlot({
+    filtered_data <- switch(input$survived_input,
+                            "Alle" = data_1,
+                            "Überlebt" = data_1[data_1$survived == "Ja", ],
+                            "Nicht-Überlebt" = data_1[data_1$survived == "Nein", ])
+    ggplot(filtered_data, aes(x = age, fill = factor(survived))) +
+      geom_histogram(binwidth = input$bin_input, color = "black") +
+      scale_x_continuous(breaks = seq(0, max(data_1$age, na.rm = TRUE), by = 1),
+                         labels = seq(0, max(data_1$age, na.rm = TRUE), by = 1)) +
+      labs(x = "Alter", y = "Anzahl", fill = "Überlebensstatus") +
+      ggtitle("Aufteilung der Anzahl der Personen, die überlebt haben oder nicht") +
+      scale_fill_manual(values = c("red", "green","red"), labels = c("Nicht-Überlebt", "Überlebt"), name = "Überlebensstatus")
+  })
+}
+shinyApp(ui, server)
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("survived_input", "Überlebensstatus:",
+                  choices = c("Alle", "Überlebt", "Nicht-Überlebt")),
+      sliderInput("bin_input", "Bin-Breite:", min = 1, max = 10, value = 1)
+    ),
+    mainPanel(
+      plotOutput("histogram")
+    )
+  )
+)
+server <- function(input, output) {
+  output$histogram <- renderPlot({
+    filtered_data <- switch(input$survived_input,
+                            "Alle" = data_1,
+                            "Überlebt" = data_1[data_1$survived == "Ja", ],
+                            "Nicht-Überlebt" = data_1[data_1$survived == "Nein", ])
+    ggplot(filtered_data, aes(x = age, fill = factor(survived))) +
+      geom_histogram(binwidth = input$bin_input, color = "black") +
+      scale_x_continuous(breaks = seq(0, max(data_1$age, na.rm = TRUE), by = 1),
+                         labels = seq(0, max(data_1$age, na.rm = TRUE), by = 1)) +
+      labs(x = "Alter", y = "Anzahl", fill = "Überlebensstatus") +
+      ggtitle("Aufteilung der Anzahl der Personen, die überlebt haben oder nicht") +
+      scale_fill_manual(values = c("red", "green","blue"), labels = c("Nicht-Überlebt", "Überlebt"), name = "Überlebensstatus")
+  })
+}
+shinyApp(ui, server)
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("survived_input", "Überlebensstatus:",
+                  choices = c("Alle", "Überlebt", "Nicht-Überlebt")),
+      sliderInput("bin_input", "Bin-Breite:", min = 1, max = 10, value = 1)
+    ),
+    mainPanel(
+      plotOutput("histogram")
+    )
+  )
+)
+server <- function(input, output) {
+  output$histogram <- renderPlot({
+    filtered_data <- switch(input$survived_input,
+                            "Alle" = data_1,
+                            "Überlebt" = data_1[data_1$survived == "Ja", ],
+                            "Nicht-Überlebt" = data_1[data_1$survived == "Nein", ])
+    ggplot(filtered_data, aes(x = age, fill = factor(survived))) +
+      geom_histogram(binwidth = input$bin_input, color = "black") +
+      scale_x_continuous(breaks = seq(0, max(data_1$age, na.rm = TRUE), by = 1),
+                         labels = seq(0, max(data_1$age, na.rm = TRUE), by = 1)) +
+      labs(x = "Alter", y = "Anzahl", fill = "Überlebensstatus") +
+      ggtitle("Aufteilung der Anzahl der Personen, die überlebt haben oder nicht") +
+      scale_fill_manual(values = c("red", "green"), labels = c("Nicht-Überlebt", "Überlebt"), name = "Überlebensstatus")
+  })
+}
+shinyApp(ui, server)
